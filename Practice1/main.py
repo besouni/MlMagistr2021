b060f8c38b2e6d2d0ec54b76ba673baddb29f0ee
Beso Tabatadze <besotabatadze84@gmail.com>
2021-03-13 15:23:53 +0400

    Practise 1

---
+++ Practice1/main.py
@@ -0,0 +1,22 @@
+# ===Exercise1_2===
+# print("Exercise_2")
+# x =int(input("X->"))
+# print(bin(x))
+
+# ===Exercise1_4===
+# print("Exercise1_4")
+# n1 = float(input("N1->"))
+# n2 = float(input("N2->"))
+# n3 = float(input("N3->"))
+# s = (n1+n2+n3)/3
+# print(round(s, 2))
+
+# ===Exercise1_7====
+# print("Exercise1_7")
+# x = float(input("X->"))
+# if x%10==0:
+#     print("რიცხვი ბოლოვდება 0-ით")
+# else:
+#     print("რიცხვი არ ბოლოვდება 0-ით")
+import random
+print(random.randint(1, 10))
