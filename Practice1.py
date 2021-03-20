3004f17794ca8f1624ee02f95c10444a882e6f73
Beso Tabatadze <besotabatadze84@gmail.com>
2021-03-20 15:38:31 +0400

    Lecture 2

--- Practice1.py
+++ Practice1.py
@@ -1,3 +1,18 @@
+# ===Exercise1_15===
+s = 0
+for x in range(2, 11, 2):
+    # print(x)
+    s = s+x
+print(s)
+
+# total = 0
+# x: int
+# for x in range(1, 101):
+#     if (x % 2 == 0):
+#         total += x
+# print(total)
+
+
 # ===Exercise1_2===
 # print("Exercise_2")
 # x =int(input("X->"))
@@ -18,5 +33,6 @@
 #     print("რიცხვი ბოლოვდება 0-ით")
 # else:
 #     print("რიცხვი არ ბოლოვდება 0-ით")
-import random
-print(random.randint(1, 10))
+# import random
+# print(random.randint(1, 10))
+# print(4-pow(2, 3)+5*2-3/2)
---
+++ Practice2.py
@@ -0,0 +1,39 @@
+# ==="Exercise 2_15===
+import random
+# numbs1 = random.sample(range(0, 5), 4)
+# print(numbs1)
+numbs = [4, 9, 7, 3, 10]
+# numbs.sort()
+numbs1 = sorted(numbs, reverse=False)
+print(numbs)
+print(numbs1)
+# print(sum(numbs))
+# print(max(numbs))
+# print(min(numbs))
+# print("sum={0}, max={1}, min={2}".format(sum(numbs), max(numbs), min(numbs)))
+# numbs.append(102)
+# print(numbs)
+# numbs.insert(2, 205)
+# print(numbs)
+# numbs.pop(3)
+# print(numbs)
+# numbs.sort()
+# numbs.reverse()
+# print("==============")
+# numbs1 = sorted(numbs, reverse=True)
+# print(numbs)
+# print(numbs1)
+
+
+# ==="Exercise 2_8===
+# def my_function1(a, b):
+#     print((a+b)/2)
+# my_function1(4, 7)
+# my_function1(7, 9)
+# my_function1(-89, 8.9)
+
+# ==="Exercise 2_6===
+# print("Exercise 2_6")
+# import random
+# for r in range(0, 10):
+#     print(random.randint(0, 100))
