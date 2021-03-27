83565d16e93f98079accf7ab89c3fc8445fbbb4d
Beso Tabatadze <besotabatadze84@gmail.com>
2021-03-27 15:18:01 +0400

    Lecture 3_1

--- Practice3.py
+++ Practice3.py
@@ -1,17 +1,27 @@
-# ==="Exercise 2_6===
+# ==="Exercise 3_4===
 import os
-print("Exercise 2_6")
-if not os.path.exists("myFiles"):
-    os.mkdir("myFiles")
-f = open("myFiles/data1.txt", "w")
-l = [*range(0, 101)]
+print("Exercise 3_4")
+if not os.path.exists("myFiles2"):
+    os.mkdir("myFiles2")
+for i in range(1, 31):
+    f = open("myFiles2/data"+str(i)+".txt", "w")
+    f.write("Programmer"+str(i))
+    f.close()
+
+# ==="Exercise 3_2===
+# import os
+# print("Exercise 3_2")
+# if not os.path.exists("myFiles"):
+#     os.mkdir("myFiles")
+# f = open("myFiles/data1.txt", "w")
+# l = [*range(0, 101)]
 # l1 = []
 # for el in range(0, 101):
 #     l1.append(el)
 # print(l1)
 # print(l)
 # f.write(str(l))
-s = ', '.join(str(item) for item in l)
+# s = ', '.join(str(item) for item in l)
 # print(s)
-f.write(s)
-f.close()
+# f.write(s)
+# f.close()
